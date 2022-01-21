cc1a7593badcffbd49c713a4efb242366e64d239
Pablo Gallina <pablogallinag@gmail.com>
2022-01-21 17:51:35 -0600

    generando el shared module y los componentes de de este mismo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
-  {
-    path: '', //'' Ruta por defecto (/)
-    component: ExampleComponent
-  }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExampleComponent
+  declarations: [ // Se importan: componentes, directivas, pipes
+    AppComponent
   ],
-  imports: [
+  imports: [ //Solo Se importan otros modulos
     BrowserModule,
     AppRoutingModule
   ],
+  //export[AppComponent], solo se exporta lo que declaramos
   providers: [],
   bootstrap: [AppComponent]
 })
