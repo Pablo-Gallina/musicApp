0bd5c760c3a7c0f008a98a0f37704ac627afeb19
Pablo Gallina <pablogallinag@gmail.com>
2022-01-21 19:36:27 -0600

    Agregando estilos y contenido al sidebar, haciendo uso del componente compartido y usando logica(.ts) en html de un componente

--- src/app/modules/home/page/home-page/home-page.component.ts
+++ src/app/modules/home/page/home-page/home-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  selector: 'app-home-page', // Etiquta html <app-home-page></app-home-page>
+  templateUrl: './home-page.component.html', // Template de este componente
+  styleUrls: ['./home-page.component.css'] // Estilos del componente
 })
 export class HomePageComponent implements OnInit {
-
+  //logica del componente
   constructor() { }
 
   ngOnInit(): void {
